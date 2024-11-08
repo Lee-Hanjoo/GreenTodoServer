c9fb72cfbb835a341e67ebfc889d0e0a4f8d5192
이한주 <gkswnehcl@gmail.com>
2024-11-08 10:49:56 +0900

    news api fix

--- api/news.js
+++ api/news.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const news = express.Router()
+const news = express.Router();
 const axios = require('axios');
 
-news.get('/', async function (req, res) {
-    const {m,s} = req.query;
-    
-    const api = await axios.get(`https://api-v2.deepsearch.com/v1/articles?api_key=02a270a26cfa43a4bb423a2c138880fc&page_size=30`)
-    res.json( api.data )
+news.get('/', async function (req, res){
+    const {m, s} = req.query;
+
+    const api = await axios.get(`https://api-v2.deepsearch.com/v1/${m}/${s}?api_key=02a270a26cfa43a4bb423a2c138880fc&page_size=30`);
+    res.json(api.data);
 })
 
-news.get('/search', async function (req, res) {
-    const {keyword} = req.query;
-    const api = await axios.get(`https://api-v2.deepsearch.com/v1/articles/?keyword=${keyword}&api_key=02a270a26cfa43a4bb423a2c138880fc&page_size=30`)
-    res.json( api.data )
+news.get('/search', async function (req, res){
+    const {m, keyword} = req.query;
+
+    const api = await axios.get(`https://api-v2.deepsearch.com/v1/${m}?keyword=${keyword}&api_key=02a270a26cfa43a4bb423a2c138880fc&page_size=30`);
+    res.json(api.data);
 })
 
 module.exports = news;
