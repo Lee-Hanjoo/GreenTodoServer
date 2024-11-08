cc12dcac3c8c61bbf3f5a0ea7fac5a8f98912596
이한주 <gkswnehcl@gmail.com>
2024-11-08 10:59:27 +0900

    news api fix

--- api/news.js
+++ api/news.js
@@ -3,16 +3,9 @@ const news = express.Router();
 const axios = require('axios');
 
 news.get('/', async function (req, res){
-    const {m, s} = req.query;
+    const {articles, section} = req.query;
 
-    const api = await axios.get(`https://api-v2.deepsearch.com/v1/${m}/${s}?api_key=02a270a26cfa43a4bb423a2c138880fc&page_size=30`);
-    res.json(api.data);
-})
-
-news.get('/search', async function (req, res){
-    const {m, keyword} = req.query;
-
-    const api = await axios.get(`https://api-v2.deepsearch.com/v1/${m}?keyword=${keyword}&api_key=02a270a26cfa43a4bb423a2c138880fc&page_size=30`);
+    const api = await axios.get(`https://api-v2.deepsearch.com/v1/${articles}/${section}?api_key=02a270a26cfa43a4bb423a2c138880fc&page_size=30`);
     res.json(api.data);
 })
 
