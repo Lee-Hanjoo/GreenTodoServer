0bb126d6d91ab92cbdce6077c8ae31f4267613a7
이한주 <gkswnehcl@gmail.com>
2024-11-12 17:17:55 +0900

    init

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 const SERVICE_ACCOUNT = process.env.SERVICE_ACCOUNT
-var serviceAccount = require(SERVICE_ACCOUNT);
+var serviceAccount = require(`${SERVICE_ACCOUNT}`);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
