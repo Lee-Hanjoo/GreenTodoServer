8c8947bb29719017c8469f372aa2824ee99635c4
이한주 <gkswnehcl@gmail.com>
2024-11-12 17:21:18 +0900

    init

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const todos = require('./api/todos');
 const news = require('./api/news');
-// const member = require('./api/member.bak')
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -12,6 +11,5 @@ app.use(bodyParser.json())
 
 app.use('/todos',todos);
 app.use('/news',news);
-// app.use('/member',member);
 
 app.listen(4000)
