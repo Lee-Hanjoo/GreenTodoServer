ff852322e89c841f4bb799fefa84fc33adaf6e06
이한주 <gkswnehcl@gmail.com>
2024-11-12 17:40:14 +0900

    init

--- api/news.js
+++ api/news.js
@@ -5,15 +5,15 @@ const apiKey = process.env.API_KEY;
 
 
 news.get('/', async function (req, res){
-    let {articles, section} = req.query;
+    const {articles, section} = req.query;
 
     const api = await axios.get(`https://api-v2.deepsearch.com/v1/${articles}/${section}?api_key=${apiKey}&page_size=30`);
     res.json(api.data);
 })
 
 news.get('/search', async function (req, res){
-    let {articles, keyword} = req.query;
-    
+    const {articles, keyword} = req.query;
+
     const api = await axios.get(`https://api-v2.deepsearch.com/v1/${articles}?keyword=${keyword}&api_key=${apiKey}&page_size=30`);
     res.json(api.data);
 })
